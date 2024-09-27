57c3ac4039f91fc9bca76f438c0f3a8bc0bafa89
AzriHarniza22 <azri.22@mhs.usk.ac.id>
2024-09-27 23:19:23 +0700

    hapus .gitkeep

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="relative flex h-screen content-center items-center justify-center pb-32 pt-16">
-      <div className="absolute top-0 w-full bg-[url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')] bg-cover bg-no-repeat bg-center">
+      <div className="absolute top-0 w-full bg-[url('https://www.google.com/url?sa=i&url=https%3A%2F%2Flifestyle.kompas.com%2Fread%2F2022%2F05%2F24%2F064520520%2Fjangan-teledor-hapus-5-data-ini-dari-komputer-kantor-sebelum-resign%3Fpage%3Dall&psig=AOvVaw0ZIh7712lkwpsYexmvHoh-&ust=1727540336878000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPjbw7PD44gDFQAAAAAdAAAAABAE')] bg-cover bg-no-repeat bg-center">
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
         <div className="max-w-8xl container relative mx-auto">
           <div className="flex flex-wrap items-center">
