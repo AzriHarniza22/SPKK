f3bf73055efd7d48584ef8b644be0c21ac36bcab
AzriHarniza22 <azri.22@mhs.usk.ac.id>
2024-09-27 23:47:02 +0700

    ubah background

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="relative flex h-screen content-center items-center justify-center pb-32 pt-16">
-      <div className="absolute top-0 w-full bg-[url('https://asset.kompas.com/crops/LXDsrfYK0sSBW_qXOZdRBMv0oVY=/0x0:1000x667/750x500/data/photo/2022/05/24/628bc3223dcd5.jpg')] bg-cover bg-no-repeat bg-center">
+      <div className="absolute top-0 w-full bg-[url('https://images.unsplash.com/photo-1516321318423-f06f85e504b3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-center">
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
         <div className="max-w-8xl container relative mx-auto">
           <div className="flex flex-wrap items-center">
